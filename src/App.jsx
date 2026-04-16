92f4da816677d59525e7d47492929e54a96a99dd
AriSyafri <diamondThink15@gmail.com>
2026-04-16 20:48:35 +0700

    use props children

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const Button = (props) => {
       className={`h-10 px-6 font-semibold text-white rounded-md ${props.variant}`} 
       type="submit"
     >
-      {props.text}
+      {props.children}
     </button>
   );
 };
@@ -16,9 +16,9 @@ function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-500">
       <div className="flex gap-x-2">
-        <Button variant="bg-red-500" text="login"></Button>
-        <Button variant="bg-slate-300" text="logout"></Button>
-        <Button variant="bg-slate-500" text="submit"></Button>
+        <Button variant="bg-red-500">logout</Button>
+        <Button variant="bg-slate-300">login</Button>
+        <Button variant="bg-slate-500">register</Button>
       </div>
     </div>
   )
