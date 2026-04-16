8d9cf308bf68ebcf0f003b6af55a9adf771f3fb1
AriSyafri <diamondThink15@gmail.com>
2026-04-16 20:29:41 +0700

    basic componen js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import React from "react"
 
-function App() {
-  
+// define a componen
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="h-10 px-6 font-semibold text-black rounded-md bg-slate-100" type="submit">
+        Buy now
+      </button>
+    );
+  }
+}
+
+// function js
+function ButtonBlack() {
+  return(
+    <button className="h-10 px-6 font-semibold text-white bg-black rounded-md" type="submit">
+      Button Black
+    </button>
+  );
+}
+
+// arrow function js
+const ButtonRed = () => {
+  return(
+    <button className="h-10 px-6 font-semibold text-white rounded-md bg-red-950" type="submit">
+      Button Red
+    </button>
+  );
+}
+
+function App() { 
   return (
-    <div className="flex justify-center bg-blue-500 min-h-screen items-center">
-      <div>Hello World</div>
+    <div className="flex items-center justify-center min-h-screen bg-blue-500">
+      <div className="flex gap-x-2">
+        <Button></Button>
+        <Button></Button>
+        <ButtonRed></ButtonRed>
+        <Button></Button>
+        <ButtonBlack></ButtonBlack>
+      </div>
     </div>
   )
 }
