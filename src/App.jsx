3169f6267bd47e1f23017e4c62b8a898b455018e
AriSyafri <diamondThink15@gmail.com>
2026-04-16 20:46:26 +0700

    use props in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,24 @@
 import React from "react"
+// reference https://legacy.reactjs.org/docs/components-and-props.html
 
-// define a componen
-class Button extends React.Component {
-  render() {
-    return (
-      <button className="h-10 px-6 font-semibold text-black rounded-md bg-slate-100" type="submit">
-        Buy now
-      </button>
-    );
-  }
-}
-
-// function js
-function ButtonBlack() {
+const Button = (props) => {
   return(
-    <button className="h-10 px-6 font-semibold text-white bg-black rounded-md" type="submit">
-      Button Black
+    <button 
+      className={`h-10 px-6 font-semibold text-white rounded-md ${props.variant}`} 
+      type="submit"
+    >
+      {props.text}
     </button>
   );
-}
-
-// arrow function js
-const ButtonRed = () => {
-  return(
-    <button className="h-10 px-6 font-semibold text-white rounded-md bg-red-950" type="submit">
-      Button Red
-    </button>
-  );
-}
+};
 
 function App() { 
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-500">
       <div className="flex gap-x-2">
-        <Button></Button>
-        <Button></Button>
-        <ButtonRed></ButtonRed>
-        <Button></Button>
-        <ButtonBlack></ButtonBlack>
+        <Button variant="bg-red-500" text="login"></Button>
+        <Button variant="bg-slate-300" text="logout"></Button>
+        <Button variant="bg-slate-500" text="submit"></Button>
       </div>
     </div>
   )
