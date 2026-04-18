b575779782920c01a7648a1bc94ea4155239159a
AriSyafri <diamondThink15@gmail.com>
2026-04-18 19:53:13 +0700

    mennggunakan render list

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,44 +1,45 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+    {
+        id: 1,
+        title: "Baju 1",
+        price: "100.000",
+        image: "/images/clothes.jpg",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque harum voluptatem maiores, iste incidunt  qui amet animi?"
+    },
+    {
+        id: 2,
+        title: "Baju 2",
+        price: "200.000",
+        image: "/images/clothes.jpg",
+        description: "iste incidunt  qui amet animi?"
+    },
+    {
+        id: 3,
+        title: "Baju 3",
+        price: "250.000",
+        image: "/images/clothes.jpg",
+        description: "iste incidunt  qui amet animiiste incidunt  qui amet animiiste incidunt  qui amet animiiste incidunt  qui amet animiiste incidunt  qui amet animiiste incidunt  qui amet animi?"
+    },
+]
+
 const ProductPage = () => {
     
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header 
-                    image="/images/clothes.jpg"
-                />
-                <CardProduct.Body title="Baju A">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque harum voluptatem maiores, iste incidunt  qui amet animi?</CardProduct.Body>
-                <CardProduct.Footer price="100.000"></CardProduct.Footer>
-            </CardProduct>
             
-            <CardProduct>
-                <CardProduct.Header 
-                    image="/images/clothes.jpg"
-                />
-                <CardProduct.Body title="Baju b">
-                    Lorem ipsum dolor, sit amet consectetur </CardProduct.Body>
-                <CardProduct.Footer price="500.000"></CardProduct.Footer>
-            </CardProduct>
-            
-            <CardProduct>
-                <CardProduct.Header 
-                    image="/images/clothes.jpg"
-                />
-                <CardProduct.Body title="Baju b">
-                    Lorem ipsum dolor, sit amet consectetur </CardProduct.Body>
-                <CardProduct.Footer price="500.000"></CardProduct.Footer>
-            </CardProduct>
-            
-            <CardProduct>
-                <CardProduct.Header 
-                    image="/images/clothes.jpg"
-                />
-                <CardProduct.Body title="Baju b">
-                    Lorem ipsum dolor, sit amet consectetur Lorem ipsum dolor, sit amet consectetur Lorem ipsum dolor, sit amet consectetur Lorem ipsum dolor, sit amet consectetur </CardProduct.Body>
-                <CardProduct.Footer price="500.000"></CardProduct.Footer>
-            </CardProduct>
+            { products.map((product) => (
+                <CardProduct>
+                    <CardProduct.Header 
+                        image={product.image}
+                    />
+                    <CardProduct.Body title={product.title}>
+                        {product.description}</CardProduct.Body>
+                    <CardProduct.Footer price={product.price}></CardProduct.Footer>
+                </CardProduct>    
+            )) }
+
         </div>
     );
 };
