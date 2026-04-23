a489c3269775ef53c327ff87660297a6f1b260d9
AriSyafri <diamondThink15@gmail.com>
2026-04-24 00:49:23 +0700

    menambah reducer

--- toolkit.js
+++ toolkit.js
@@ -5,7 +5,6 @@ const { configureStore, createAction, createReducer} = toolkit;
 
 const addToCart = createAction("ADD_TO_CART"); 
 
-
 const cartReducer = createReducer([], (builder) => {
     builder.addCase(addToCart, (state, action) => {
         // state.cart = [...state.cart, action.payload],
@@ -13,8 +12,17 @@ const cartReducer = createReducer([], (builder) => {
     });
 });
 
+const login = createAction("CREATE_SESSION");
+
+const loginReducer = createReducer({ status: false }, (builder) => {
+    builder.addCase(login, (state, action) => {
+        state.status = true;
+    });
+});
+
 const store = configureStore({
     reducer: {
+        login: loginReducer,
         cart: cartReducer,
     },
 });
@@ -29,3 +37,5 @@ const action1 = addToCart({id: 10, qty: 5});
 store.dispatch(action1);
 store.dispatch(addToCart({id: 15, qty: 5}));
 store.dispatch(addToCart({id: 16, qty: 3}));
+
+store.dispatch(login());
